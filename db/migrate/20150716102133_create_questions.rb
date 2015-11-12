04863ea24adbb8f40354ea5d189810ced9cdb682
raidersofthenight <ianemailaddress@gmail.com>
2015-11-11 19:36:23 -0800

    create db field for explanation

--- db/migrate/20150716102133_create_questions.rb
+++ db/migrate/20150716102133_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :text
+      t.string :explanation
 
       t.timestamps
       t.references :survey, index: true

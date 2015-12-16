b174b2a647b24d429997def4a88e5c76deef2dd2
raidersofthenight <ianemailaddress@gmail.com>
2015-12-16 02:42:01 -0800

    hate css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -134,8 +134,7 @@ $(document).ready(function() {
 
     request.done(function(serverData) {
       // TODO: LOGIC TO REMOVE FORM AND DISPLAY NEWLY CREATED QUESTION
-      // console.log("trace complete");
-      // console.log(data);
+
       $('#add_question')[0].reset();
       $('.new-question-form').animate({
         top: '+=40'

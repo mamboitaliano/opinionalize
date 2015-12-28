e565d9f42a2c65baf567c08c79263a007f071048
raidersofthenight <ianemailaddress@gmail.com>
2015-12-28 03:21:13 -0800

    style answer choice dialog window

--- db/migrate/20150716102133_create_questions.rb
+++ db/migrate/20150716102133_create_questions.rb
@@ -1,8 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :text
-      t.string :explanation
+      t.string :text			# TODO: define acceptable chars
+      t.string :explanation		# TODO: define acceptable chars
+      t.integer :resp_type 		# TODO: define acceptable response types
+      t.string :resp_choices	# TODO: define acceptable response choices
 
       t.timestamps
       t.references :survey, index: true
